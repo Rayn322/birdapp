bc6a7d8583ad71a1addb8d1256fd51ffe0a01a43
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2022-11-19 18:03:06 -0800

    Run prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
+import { type AppType } from 'next/app';
+import { type Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
 
-import { trpc } from "../utils/trpc";
+import { trpc } from '../utils/trpc';
 
-import "../styles/globals.css";
+import '../styles/globals.css';
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
-  pageProps: { session, ...pageProps },
+  pageProps: { session, ...pageProps }
 }) => {
   return (
     <SessionProvider session={session}>
