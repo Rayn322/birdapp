98d77bd9c2411b0639242625c89e755cc0a662c8
Ryan Lauderbach <16494880+Rayn322@users.noreply.github.com>
2022-11-20 17:48:58 -0800

    Set font to Inter with @next/font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,28 @@
 import { type AppType } from 'next/app';
 import { type Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
-
 import { trpc } from '../utils/trpc';
+import { Inter } from '@next/font/google';
 
 import '../styles/globals.css';
+
+const inter = Inter();
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps }
 }) => {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </>
   );
 };
 
